f67b68e24c4fb06d100ce2b624033b19a469c583
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-07-12 17:10:15 +0500

    minor fix

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,7 +9,6 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = React.memo(props => {
-  console.log("render");
   const postsElements = [...props.posts]
     .reverse()
     .map(p => (
