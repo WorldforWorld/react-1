a0e170aeb1dcd9fb1e29d384ce7b24c97af7caa6
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-07-25 17:17:25 +0500

    minor fix

--- src/redux/sidebar-reducer.ts
+++ src/redux/sidebar-reducer.ts
@@ -1,6 +1,9 @@
 const initialState = {};
 type InitialStateType = typeof initialState;
-const sidebarReducer = (state = initialState, action: any) => {
+const sidebarReducer = (
+  state = initialState,
+  action: any
+): InitialStateType => {
   return state;
 };
 export default sidebarReducer;
